a4784d5deddfe3fa4aa9acc1531df15096189eb0
Tung Nguyen <tungnguyen@vsoft.vn>
2014-07-14 10:40:32 +0700

    add constant app.js

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,23 +1,30 @@
 'use strict';
 
 angular.module('vClientApp', [
-  'ngCookies',
-  'ngResource',
-  'ngSanitize',
-  'ngRoute',
-  'vClientApp.restful',
-  'vClientApp.logger',
-  'vClientApp.baseModel',
-  'vClientApp.fetchData',
-  'vClientApp.socket',
-  'vClientApp.auth'
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngRoute',
+    'vClientApp.restful',
+    'vClientApp.logger',
+    'vClientApp.baseModel',
+    'vClientApp.fetchData',
+    'vClientApp.socket',
+    'vClientApp.auth'
 ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+    .constant('appConfig', {
+        deviceId: (window.device) ? device.uuid.toLowerCase() : 'what.do.namehihi????',
+        defaultPass: 'defaultPassword',
+        name: 'iTaxi',
+        apiHost: 'http://nodejs.vn:1212', // taxigo.vn:9697
+        mediaHost: 'http://vsoft.vn:1235'
+    })
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/main.html'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    });
